900fcd25a455eb382ffd2faa99991167851c7af4
RezaMrtz <reza.mrtz@gmail.com>
2019-11-02 15:57:18 +0330

    Some issues resolved

--- generate_faker.py
+++ generate_faker.py
@@ -1,20 +1,20 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULES','first_project.settings')
-
 import django
+import random
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "first_project.settings")
 django.setup()
 
-import random
 from first_app.models import AccessRecord, WebPage, Topic 
 from faker import Faker 
-
 
 fakegen = Faker()
 topics = ['Search', 'Dating Apps', 'Games', 'News']
 
 def add_topic():
     t = Topic.objects.get_or_create(topic_name=random.choice(topics))[0]
-    return t.save()
+    t.save()
+    return t 
 
 def populate(N=5):
 
@@ -23,13 +23,13 @@ def populate(N=5):
         top = add_topic()
 
         # Faker 
-        faker_ulr = fakegen.url()
-        fake_data = fakegen.date()
+        fake_url = fakegen.url()
+        fake_date = fakegen.date()
         fake_name = fakegen.company()
 
         # Create the new webpage entry 
-        webpg = WebPage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
-
+        webpg = WebPage.objects.get_or_create(topic=top, name=fake_name, url=fake_url)[0]
+        
         # Create a fake access record for that webage
         acc_rec = AccessRecord.objects.get_or_create(name=webpg, date=fake_date)
 
